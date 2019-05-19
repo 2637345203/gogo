6eb4145526329472684105231c334b6d792235a5
Your Name <you@example.com>
2019-05-19 13:48:29 +0800

    更新

---
+++ src/main/java/com/rk/TestB.java
@@ -0,0 +1,24 @@
+package com.rk;
+
+/*
+　　へ　　　　　／|
+　/＼7　　∠＿/
+　/　│　　 ／　／
+　│　Z ＿,＜　／　　    /`ヽ
+　│　　　　　ヽ　　    /　〉
+　 Y　　　　　`　    /　/
+　ｲ●　､　●　　⊂⊃  〈　/
+　()　 へ　　　　|　＼〈
+　　>ｰ ､_　 ィ　 │ ／／
+　 / へ　　 /　ﾉ＜| ＼＼
+　 ヽ_ﾉ　　(_／　 │／／
+　　7　　　　　　　|／
+　　＞―r￣￣`ｰ―＿
+ * @Author: runningMan
+ * @Date: 2019/5/19 13:47*/
+
+public class TestB {
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+}
